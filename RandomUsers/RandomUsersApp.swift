95384f887fc507a62d3ecae171a6d0aa50fb151b
Yasser Bal <yasserbal@icloud.com>
2023-08-31 23:45:47 +0200

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Yasser Bal on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
